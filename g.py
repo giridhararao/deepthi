f0adac7edecb9a0b549a03b16a1c95e3be7d3494
giridhararao <www.giridhararao@gmail.com>
2018-07-12 14:59:29 +0530

    Update g.py

--- g.py
+++ g.py
@@ -1,3 +1,12 @@
-a=input()
-b=list(set(a))
+ia=raw_input()
+l=len(ia)
+a=[]
+b=[]
+for j in range(0,l):
+     a.append(ia[j])
+for j in a:
+      if j in b:
+            break
+      if j not in b:
+            b.append(j)
 print(len(b))
